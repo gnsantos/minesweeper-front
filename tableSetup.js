37d2ebce7fd7aea1b12393b8b563ba6d3fde4e47
Gervasio Santos <gervasio.neto94@gmail.com>
2018-12-15 03:44:15 -0200

    Adding API call to make a move in the game and redraw the board. Says "Game Over" when clicking bomb.

--- tableSetup.js
+++ tableSetup.js
@@ -2,6 +2,50 @@ const BASE_URL = "http://whispering-atoll-52291.herokuapp.com";
 var BOARD_ID = null;
 
 $(document).ready(function(){
+	var startGame = function(successCallback, errorCallback) {
+    var route = '/start';
+    $.ajax({
+			'type': 'GET',
+			'url': BASE_URL + route,
+			"headers": {
+					'Accept': 'application/json',
+					'Content-Type': 'application/json',
+					'Access-Control-Allow-Origin': '*',
+					'Access-Control-Allow-Methods': 'GET, POST, PUT'
+			},
+			'crossDomain': true,
+			'dataType': 'json',
+			'success': successCallback,
+      'error': errorCallback
+		});
+  };
+
+	var makeMove = function(row, col, successCallback, errorCallback) {
+		var route = '/move';
+
+		var req = {
+			'row': row,
+			"col": col,
+			'board_id': BOARD_ID
+		};
+
+		$.ajax({
+			'type': 'POST',
+			'url': BASE_URL + route,
+			"headers": {
+					'Accept': 'application/json',
+					'Content-Type': 'application/json',
+					'Access-Control-Allow-Origin': '*',
+					'Access-Control-Allow-Methods': 'GET, POST, PUT'
+			},
+			'data': JSON.stringify(req),
+			'crossDomain': true,
+			'dataType': 'json',
+			'success': successCallback,
+			'error': errorCallback
+		});
+	};
+
 	var buildTableHTML = function(rows, cols, cells) {
 		var range_rows = [...Array(rows).keys()];
 		var range_cols = [...Array(cols).keys()];
@@ -24,30 +68,13 @@ $(document).ready(function(){
 		return rowHtml;
 	};
 
-	var startGame = function(successCallback, errorCallback) {
-    var route = '/start';
-    $.ajax({
-			'type': 'GET',
-			'url': BASE_URL + route,
-			"headers": {
-					'Accept': 'application/json',
-					'Content-Type': 'application/json',
-					'Access-Control-Allow-Origin': '*',
-					'Access-Control-Allow-Methods': 'GET, POST, PUT'
-			},
-			'crossDomain': true,
-			'dataType': 'json',
-			'success': successCallback,
-      'error': errorCallback
-		});
-  };
-
 	$('#start-btn').on('click', function(){
 		var callback = function(response){
 			var cells = response.cells;
 			var rows = response.rows;
 			var cols = response.cols;
-			boardID = response.id;
+
+			BOARD_ID = response.id;
 
 			var tableContentHtml = buildTableHTML(rows, cols, cells);
 			$('#table').html(tableContentHtml);
@@ -56,5 +83,32 @@ $(document).ready(function(){
 		}
 
 		startGame(callback, function(){alert('error')})
+	});
+
+	$('table').on('click', 'td', function(){
+		var colIndex = $(this).parent().children().index($(this));
+		var rowIndex = $(this).parent().parent().children().index($(this).parent());
+
+		var successCallback = function(response){
+			console.log(response);
+			var cells = response.cells;
+			var rows = response.rows;
+			var cols = response.cols;
+
+			var tableContentHtml = buildTableHTML(rows, cols, cells);
+			$('#table').html(tableContentHtml);
+
+			if (response.status === "lost") {
+				alert('You clicked a bomb!');
+				$("#status").html("Game Lost");
+			}
+		};
+
+		var errorCallback = function(resp){
+			console.log(resp);
+			alert('Invalid Move!');
+		};
+
+		makeMove(rowIndex, colIndex, successCallback, errorCallback);
 	});
 });
